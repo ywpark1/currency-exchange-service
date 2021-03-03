b1f4e69a7c4bb3bcce35c42b578fc4ac8082b2c9
Yeon Woo Park <ywoo1234@gmail.com>
2021-03-03 00:25:49 -0500

    Add resillience4j and apply sample example with 'retry'

---
+++ src/main/java/com/ywparkdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,30 @@
+package com.ywparkdev.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("Sample API is called");
+        ResponseEntity<Object> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+                null, String.class);
+
+        return forEntity.toString();
+    }
+
+    public String hardcodedResponse(Exception ex) {
+        return "fallback-response";
+    }
+}
