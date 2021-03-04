034dd3039993e1178c3b7b937f428c72d0ff9425
Yeon Woo Park <ywoo1234@gmail.com>
2021-03-03 22:32:43 -0500

    Add simple CircuitBreaker

--- src/main/java/com/ywparkdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/ywparkdev/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.ywparkdev.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +17,9 @@ public class CircuitBreakerController {
 
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
     public String sampleApi() {
         logger.info("Sample API is called");
         ResponseEntity<Object> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
